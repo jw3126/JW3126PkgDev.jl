f80dac07437d07d7b8ca73765ace80538cc1ed37
Jan Weidner <jw3126@gmail.com>
2019-12-11 22:14:36 +0100

    init

--- src/JW3126PkgDev.jl
+++ src/JW3126PkgDev.jl
@@ -1,5 +1,19 @@
 module JW3126PkgDev
+export gen
+using PkgTemplates
 
-greet() = print("Hello World!")
+const DEFAULT_TEMPLATE = Template(;
+       license="MIT",
+       ssh=true,         
+       plugins=[
+           TravisCI(),   
+           Codecov(),    
+           GitHubPages(),
+       ],
+   )
+
+function gen(name, template=DEFAULT_TEMPLATE)
+    PkgTemplates.generate(template, name)
+end
 
 end # module
